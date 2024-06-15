e9b1624b625e6f3f1745952aae1c1005e45a0332
kevincitoo08 <kevin.valenzuela.39108@gmail.com>
2024-06-14 21:21:38 -0300

    Create simulado.cpp

---
+++ simulado.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "juegos.h"
+#include "funciones.h"
+#include "menu.h"
+using namespace std;
+
+
+
+void ModoUnJugadorSimulado(){
+
+    const int TAM = 6;
+    int tirada[TAM];
+    int tirada2[TAM];
+    int ronda = 1;
+    int PuntosAct = 0;
+    int puntos = 0;
+    string nombre;
+
+        cout << "Ingrese su nombre de jugador: ";
+        cin.ignore();
+
+    getline(cin, nombre);
+        cout << " " <<endl;
+
+    system("cls");
+
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+        cout << "                                               COMIENZA LA RONDA #" << ronda <<endl;
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+
+    while (PuntosAct <= 100){
+    for (int i = 0; i < 3; i++){
+        cout << "TURNO DE " << nombre << "     |    RONDA #" << ronda << "    |     PUNTAJE ACTUAL: " << PuntosAct << endl;
+
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+
+    escribirTirada (tirada, TAM);
+    ordenarVector(tirada, TAM);
+    Escalera (tirada, TAM);
+    puntos = SumaDeDados (tirada, TAM);
+    Sexteto6(tirada, TAM);
+    PuntosAct += puntos;
+    if (Escalera(tirada, TAM) == true){
+        system("cls");
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+        cout << "" <<endl;
+        cout << "                                                    ESCALERA GANASTE!" << endl;
+        cout << "" <<endl;
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+        system("pause");
+        menu();}
+
+    if (Sexteto6(tirada, TAM) == true){
+        PuntosAct = 0;
+    }
+
+
+
+
+
+        cout << "LA TIRADA SUMA: " << PuntosAct << " PUNTOS" <<endl;
+
+    PressEnter();
+
+    if (PuntosAct > 100 ){
+        break;
+    }
+    }
+    system("cls");
+    if (PuntosAct < 100){
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+        cout << " " <<endl;
+        cout << " " <<endl;
+        cout << "                                           LA PUNTUACION DE LA RONDA # " << ronda << " es: " << PuntosAct<< endl;
+        cout << " " <<endl;
+        cout << " " <<endl;
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+    ronda++;
+    system("pause");
+    system("cls");
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+        cout << "                                               COMIENZA LA RONDA #" << ronda <<endl;
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+    }
+    }
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+        cout << " " <<endl;
+        cout << " " <<endl;
+        cout << "                           FELICIDADES HA GANADO!!! =)" <<endl;
+        cout << "                           SU PUNTUACION ES: " << PuntosAct <<endl;
+        cout << " " <<endl;
+        cout << " " <<endl;
+        cout << "------------------------------------------------------------------------------------------------------------------------";
+    system("pause");
+
+
+        cout << "                           FELICIDADES HA GANADO!!! =)" <<endl;
+        cout << "                           SU PUNTUACION ES: " << PuntosAct <<endl;
+
+}
