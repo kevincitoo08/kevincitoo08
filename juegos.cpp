9408254b74b070da2ea542450dcfd84a74c1a6f1
Fede <fedeluismenendez@gmail.com>
2024-06-17 23:35:30 -0300

    Update juegos.cpp

--- juegos.cpp
+++ juegos.cpp
@@ -1,80 +1,96 @@
 #include <iostream>
 #include "juegos.h"
 #include "funciones.h"
+#include "menu.h"
 using namespace std;
 
-void ModoUnJugador(){
+void ModoUnJugador()
+{
     const int TAM = 6;
     int tirada[TAM];
     int ronda = 1;
     int PuntosAct = 0;
+    int puntos = 0;
     string nombre;
 
-        cout << "Ingrese su nombre de jugador: ";
-        cin.ignore();
+    cout << "Ingrese su nombre de jugador: ";
+    cin.ignore();
 
     getline(cin, nombre);
-        cout << " " <<endl;
+    cout << " " <<endl;
 
     system("cls");
 
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-        cout << "                                               COMIENZA LA RONDA #" << ronda <<endl;
-        cout << "------------------------------------------------------------------------------------------------------------------------";
+    cout << "------------------------------------------------------------------------------------------------------------------------";
+    cout << "                                               COMIENZA LA RONDA #" << ronda <<endl;
 
-    while (PuntosAct <= 100){
-    for (int i = 0; i < 3; i++){
-        cout << "TURNO DE " << nombre << "     |    RONDA #" << ronda << "    |     PUNTAJE ACTUAL: " << PuntosAct << endl;
+    while (PuntosAct <= 100)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cout << "------------------------------------------------------------------------------------------------------------------------";
+            cout << "TURNO DE " << nombre << "     |    RONDA #" << ronda << "    |     PUNTAJE ACTUAL: " << PuntosAct << endl;
+            cout << "------------------------------------------------------------------------------------------------------------------------";
 
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-
-    generarTirada(tirada, TAM);
-    mostrarTirada(tirada, TAM);
-    Sexteto (tirada, TAM, 6);
-    Sexteto6(tirada, TAM);
-    int puntos = SumaDeDados (tirada, TAM);
-    PuntosAct += puntos;
-
-        cout << "LA TIRDADA SUMA: " << puntos << " PUNTOS" <<endl;
-
-    PressEnter();
-
-    if (PuntosAct > 100){
-        i = 4;
+            generarTirada(tirada, TAM);
+            mostrarTirada(tirada, TAM);
+            ordenarVector(tirada, TAM);
+            Escalera (tirada, TAM);
+            puntos = SumaDeDados (tirada, TAM);
+            Sexteto6(tirada, TAM);
+            PuntosAct += puntos;
+            if (Escalera(tirada, TAM) == true)
+            {
+                system("cls");
+                cout << "------------------------------------------------------------------------------------------------------------------------";
+                cout << "" <<endl;
+                cout << "                                                    ESCALERA GANASTE!" << endl;
+                cout << "" <<endl;
+                cout << "------------------------------------------------------------------------------------------------------------------------";
+                system("pause");
+                menu();
+            }
+            if (Sexteto6(tirada, TAM) == true)
+            {
+                puntos = 0;
+                PuntosAct = 0;
+                cout << "SEXTETO DE SEIS! SE REINICIA EL PUNTAJE!"<<endl;
+            }
+            else
+            {
+                cout << "LA TIRADA SUMA: " << puntos << " PUNTOS" <<endl;
+                system("pause");
+            }
+            if (PuntosAct > 100 )
+            {
+                break;
+            }
+        }
+        system("cls");
+        if (PuntosAct < 100)
+        {
+            cout << "------------------------------------------------------------------------------------------------------------------------";
+            cout << " " <<endl;
+            cout << " " <<endl;
+            cout << "                                           LA PUNTUACION DE LA RONDA # " << ronda << " es: " << PuntosAct<< endl;
+            cout << " " <<endl;
+            cout << " " <<endl;
+            cout << "------------------------------------------------------------------------------------------------------------------------";
+            ronda++;
+            system("pause");
+            system("cls");
+            cout << "------------------------------------------------------------------------------------------------------------------------";
+            cout << "                                               COMIENZA LA RONDA #" << ronda <<endl;
+            cout << "------------------------------------------------------------------------------------------------------------------------";
+        }
     }
-    }
-    system("cls");
-    if (PuntosAct < 100){
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-        cout << " " <<endl;
-        cout << " " <<endl;
-        cout << "                                           LA PUNTUACION DE LA RONDA # " << ronda << " es: " << PuntosAct<< endl;
-        cout << " " <<endl;
-        cout << " " <<endl;
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-    ronda++;
+    cout << "------------------------------------------------------------------------------------------------------------------------";
+    cout << " " <<endl;
+    cout << " " <<endl;
+    cout << "                           FELICIDADES HA GANADO!!! =)" <<endl;
+    cout << "                           SU PUNTUACION ES: " << PuntosAct <<endl;
+    cout << " " <<endl;
+    cout << " " <<endl;
+    cout << "------------------------------------------------------------------------------------------------------------------------";
     system("pause");
-    system("cls");
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-        cout << "                                               COMIENZA LA RONDA #" << ronda <<endl;
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-    }
-    }
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-        cout << " " <<endl;
-        cout << " " <<endl;
-        cout << "                           FELICIDADES HA GANADO!!! =)" <<endl;
-        cout << "                           SU PUNTUACION ES: " << PuntosAct <<endl;
-        cout << " " <<endl;
-        cout << " " <<endl;
-        cout << "------------------------------------------------------------------------------------------------------------------------";
-    system("pause");
-
-
-        cout << "                           FELICIDADES HA GANADO!!! =)" <<endl;
-        cout << "                           SU PUNTUACION ES: " << PuntosAct <<endl;
-
-}
-
-void ModoDosJugadores(){
 }
