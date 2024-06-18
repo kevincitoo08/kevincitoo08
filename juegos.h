b2c06bafa9d0c705b23dea8a0455059171487310
Fede <fedeluismenendez@gmail.com>
2024-06-17 23:34:32 -0300

    Update juegos.h

--- juegos.h
+++ juegos.h
@@ -1,9 +1,10 @@
 #ifndef JUEGOS_H_INCLUDED
 #define JUEGOS_H_INCLUDED
 
-
-void ModoDosJugadores();
+void ModoSimuldado();
 void ModoUnJugador();
-
+void ModoDosJugadores();
+void ModoUnJugadorSimulado();
+void ModoSimuldado();
 
 #endif // JUEGOS_H_INCLUDED
